503f6b2fc3ced920a8caf98e461e5e84a157d222
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2022-02-05 20:54:01 +0100

    added discovering bt devices

---
+++ src/components/DeviceCard.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+import {Device} from 'react-native-ble-plx';
+
+const DeviceCard = ({device}) => {
+  console.log(device);
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Text style={styles.textBasic}>{`Id: ${device.id}`}</Text>
+        <Text style={styles.textBasic}>{`Name: ${device.name}`}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#D3D3D3',
+    marginTop: 10,
+    marginBottom: 10,
+    shadowOpacity: 0.4,
+    padding: 2
+  },
+  textBasic: {
+    color: 'black',
+  },
+});
+
+export default DeviceCard;
--- src/navigation/screens/SettingsScreen.js
+++ src/navigation/screens/SettingsScreen.js
@@ -1,18 +1,49 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, FlatList} from 'react-native';
+import {BleManager} from 'react-native-ble-plx';
+
+import DeviceCard from '../../components/DeviceCard';
+
+const manager = new BleManager();
+var devices = [];
 
 const SettingsScreen = () => {
-    return(
-        <View>
-            <Text style={styles.textBasic}>Settings Screen</Text>
-        </View>
-    )
-}
+  const [isLoading, setIsLoading] = useState(false);
+
+  const scanDevices = () => {
+    setIsLoading(true);
+
+    manager.startDeviceScan(null, null, (error, scannedDevice) => {
+      if (error) {
+        console.warn(error);
+      }
+      if (scannedDevice && !devices.find((dev) => dev.id === scannedDevice.id)) {
+        console.log(scannedDevice)
+        devices = [
+          ...devices,
+          {id: scannedDevice.id, name: scannedDevice.name},
+        ];
+      }
+    });
+
+    setTimeout(() => {
+      manager.stopDeviceScan();
+      setIsLoading(false);
+    }, 5000);
+  };
+
+  return (
+    <View>
+      <Button title="Scan bluetooth devices" onPress={scanDevices} />
+      {devices.map((device) => <DeviceCard device={device} />)}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    textBasic: {
-        color: 'black',
-    }
-})
+  textBasic: {
+    color: 'black',
+  },
+});
 
-export default SettingsScreen
+export default SettingsScreen;
